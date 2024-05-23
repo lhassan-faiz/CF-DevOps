468d388365ece79bc859215cc6146ca0830b8132
lhassan-faiz <lhassanfaiz01@gmail.com>
2024-05-23 14:25:03 +0100

    voila la commit de dev

--- maven/src/main/java/maven/Hello.java
+++ maven/src/main/java/maven/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args)
     {
         System.out.println("Bonjour et bon courage dans votre examen en DevOps !!");
+        System.out.println("je suis le dev et j ai ajouter ce ligne ");
     }
 
 }
